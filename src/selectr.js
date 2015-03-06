c507f1609a74e3c58879f3459b1a7dcec9b924ce
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2015-03-06 14:30:57 -0300

    Initial commit

---
+++ src/selectr.js
@@ -0,0 +1,35 @@
+(function(global, d) {
+  'use strict';
+
+  var Selectr = function(params) {
+    if (!params) throw 'You need to pass a few parameters!';
+    if (! ('id' in params)) throw 'You need to pass a \'id\' parameter to find select!';
+    if (! ('itens' in params)) throw 'You need to pass a \'itens\' parameter to find select itens!';
+    if (! ('onChange' in params)) throw 'You need to pass a \'onChange\' function parameter!';
+    if ((typeof params.onChange !== 'function')) throw 'onChange callback need be a function!';
+    if (! d.querySelector(params.id)) throw 'Select not matched!';
+
+    this.process(params);
+  };
+
+  Selectr.prototype.process = function(params) {
+    var select = d.querySelector(params.id),
+        listItem,
+        onChangeHandler;
+
+    listItem = select.querySelectorAll(params.itens);
+    onChangeHandler = function(e) {
+      if (!/selected/g.test(this.className))
+        params.onChange.call(this);
+      e.preventDefault();
+    };
+
+    for (var i = 0; i <  listItem.length; i++) {
+      var item = listItem[i];
+      item.addEventListener('click', onChangeHandler);
+      item.addEventListener('touchend', onChangeHandler);
+    }
+  };
+
+  global.Selectr = Selectr;
+})(window, document);
